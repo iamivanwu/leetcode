6a370c28dfb49a4abb7411b9c1ac842e1666520b
WuIFan <denny91002@gmail.com>
2022-03-03 09:27:15 +0800

    Modified: refactor Arithmetic Slices

--- Solutions/Arithmetic_Slices.py
+++ Solutions/Arithmetic_Slices.py
@@ -1,19 +1,16 @@
 from typing import List
 class Solution:
-    def numberOfArithmeticSlices(self, A: List[int]) -> int:
-        if len(A) < 3:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        dp,res = 0,0
+        if len(nums) < 3:
             return 0
-        def cal(n):
-            return (1+n-2)*(n-2)//2
-        res,d,count = 0,-1,0
-        for i in range(1,len(A)):
-            if A[i] - A[i-1] == d:
-                count += 1
+        d = nums[1] - nums[0]
+        for i in range(2, len(nums)):
+            if nums[i] - nums[i-1] == d:
+                dp += 1
+                res += dp
             else:
-                if count >= 2:
-                    res += cal(count+1)
-                d = A[i] - A[i-1]
-                count = 1
-        if count >= 2:
-            res += cal(count+1)
+                dp = 0
+            d = nums[i] - nums[i-1]
         return res
+print(Solution().numberOfArithmeticSlices([1,2,3,4,5,6]))
